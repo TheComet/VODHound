9f6e665dee41f4306e79c846e423bc02df47ebc7
TheComet <alex.murray@gmx.ch>
2023-10-09 10:14:00 +0200

    Fix merge conflicts

--- iupgfx/src/iup_gfxcanvas_win32_dx11.c
+++ iupgfx/src/iup_gfxcanvas_win32_dx11.c
@@ -10,7 +10,7 @@
 
 #include "iup.h"
 #include "iupcbs.h"
-#include "iup3d.h"
+#include "iupgfx.h"
 
 #include "iup_attrib.h"
 #include "iup_class.h"
@@ -21,29 +21,30 @@ struct Vertex
 };
 
 static struct Vertex quad[6] = {
-    { 0.0, 0.0 },
-    { 1.0, 0.0 },
-    { 1.0, 1.0 },
-    { 0.0, 0.0 },
-    { 0.0, 1.0 },
-    { 1.0, 1.0 }
+    { -1.0, -1.0 },
+    {  1.0,  1.0 },
+    {  1.0, -1.0 },
+    { -1.0, -1.0 },
+    { -1.0,  1.0 },
+    {  1.0,  1.0 },
 };
 static D3D11_INPUT_ELEMENT_DESC quad_layout[] =
 {
     { "POSITION", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 }
 };
 
-static const char* vs_yuv420 =
-"float4 vs_yuv420(float3 pos_in : POSITION) : SV_POSITION\n"
-"{\n"
-"   return float4(pos_in.xy, 0.5, 1.0);\n"
-"}\n";
+static const char* vs_quad = "\
+float4 vs_yuv420(float2 pos_in : POSITION) : SV_POSITION\n\
+{\n\
+   return float4(pos_in.xy, 0.0, 1.0);\n\
+}";
 
-static const char* ps_yuv420 =
-"float4 ps_yuv420(float4 pos : SV_POSITION) : SV_TARGET\n"
-"{\n"
-"   return float4(1.0, 0.0, 0.0, 1.0);\n"
-"}\n";
+static const char* ps_yuv420 = "\
+float4 ps_yuv420(float4 pos : SV_POSITION) : SV_TARGET\n\
+{\n\
+    float2 uv = pos.xy * 0.5 + 0.5;\n\
+    return float4(uv/500, 0.0, 1.0);\n\
+}";
 
 typedef struct Context3D
 {
@@ -66,20 +67,11 @@ static int CreateDX11Context(Ihandle* ih, Context3D* ctx)
     {
         ID3D11Device* base_dev;
         ID3D11DeviceContext* base_devcon;
-        DXGI_SWAP_CHAIN_DESC scd;
         D3D_FEATURE_LEVEL feature_levels[] = { D3D_FEATURE_LEVEL_11_0 };
         UINT creation_flags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
 #if defined(DEBUG)
         creation_flags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
-
-        ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
-        scd.BufferCount = 1;                                    /* one back buffer */
-        scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     /* use 32-bit color */
-        scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      /* how swap chain is to be used */
-        scd.OutputWindow = ctx->hWnd;                           /* the window to be used */
-        scd.SampleDesc.Count = 4;                               /* how many multisamples */
-        scd.Windowed = TRUE;                                    /* windowed/full-screen mode */
 
         hResult = D3D11CreateDevice(NULL,
             D3D_DRIVER_TYPE_HARDWARE,
@@ -186,7 +178,7 @@ static int CreateDX11Context(Ihandle* ih, Context3D* ctx)
     ID3D11DeviceContext_OMSetRenderTargets(ctx->devcon, 1, &ctx->backbuffer, NULL);
 
     ID3DBlob *vs, *ps, *errors;
-    if (D3DCompile(vs_yuv420, strlen(vs_yuv420), "vs_yuv420", NULL, NULL, "vs_yuv420", "vs_4_0", 0, 0, &vs, &errors) != 0)
+    if (D3DCompile(vs_quad, strlen(vs_quad), "vs_yuv420", NULL, NULL, "vs_yuv420", "vs_4_0", 0, 0, &vs, &errors) != 0)
     {
         iupAttribSet(ih, "ERROR", "Failed to create device and swap chain.");
         iupAttribSetStr(ih, "LASTERROR", ID3D10Blob_GetBufferPointer(errors));
@@ -211,8 +203,8 @@ static int CreateDX11Context(Ihandle* ih, Context3D* ctx)
 
     D3D11_BUFFER_DESC bd;
     ZeroMemory(&bd, sizeof(bd));
-    bd.ByteWidth = sizeof(struct Vertex) * 6;      /* size of one face */
-    bd.Usage = D3D11_USAGE_DEFAULT;              /* write access access by CPU and GPU */
+    bd.ByteWidth = sizeof(struct Vertex) * 6;      /* Size of vertex buffer */
+    bd.Usage = D3D11_USAGE_DEFAULT;                /* write access access by CPU and GPU */
     bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;       /* use as a vertex buffer */
 
     D3D11_SUBRESOURCE_DATA sd;
@@ -222,6 +214,74 @@ static int CreateDX11Context(Ihandle* ih, Context3D* ctx)
 
     if (ID3D11Device_CreateBuffer(ctx->dev, &bd, &sd, &ctx->quad) != 0)
         log_err("create buffer failed\n");
+
+    // Create Constant Buffer
+    struct Constants
+    {
+        float2 pos;
+        float2 paddingUnused; // color (below) needs to be 16-byte aligned! 
+        float4 color;
+    };
+
+    ID3D11Buffer* constantBuffer;
+    {
+        D3D11_BUFFER_DESC constantBufferDesc = {};
+        // ByteWidth must be a multiple of 16, per the docs
+        constantBufferDesc.ByteWidth = sizeof(Constants) + 0xf & 0xfffffff0;
+        constantBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+        constantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+        constantBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+        HRESULT hResult = d3d11Device->CreateBuffer(&constantBufferDesc, nullptr, &constantBuffer);
+        assert(SUCCEEDED(hResult));
+    }
+
+    D3D11_SAMPLER_DESC samplerDesc;
+    ZeroMemory(&samplerDesc, sizeof samplerDesc);
+    samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+    samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_BORDER;
+    samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_BORDER;
+    samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_BORDER;
+    samplerDesc.BorderColor[0] = 1.0f;
+    samplerDesc.BorderColor[1] = 1.0f;
+    samplerDesc.BorderColor[2] = 1.0f;
+    samplerDesc.BorderColor[3] = 1.0f;
+    samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+
+    ID3D11SamplerState* samplerState;
+    ID3D11Device_CreateSamplerState(ctx->dev, &samplerDesc, &samplerState);
+
+    // Load Image
+    int texWidth, texHeight, texNumChannels;
+    int texForceNumChannels = 4;
+    unsigned char* testTextureBytes = stbi_load("testTexture.png", &texWidth, &texHeight,
+        &texNumChannels, texForceNumChannels);
+    assert(testTextureBytes);
+    int texBytesPerRow = 4 * texWidth;
+
+    // Create Texture
+    D3D11_TEXTURE2D_DESC textureDesc;
+    ZeroMemory(&textureDesc, sizeof textureDesc);
+    textureDesc.Width = texWidth;
+    textureDesc.Height = texHeight;
+    textureDesc.MipLevels = 1;
+    textureDesc.ArraySize = 1;
+    textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+    textureDesc.SampleDesc.Count = 1;
+    textureDesc.Usage = D3D11_USAGE_IMMUTABLE;
+    textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+
+    D3D11_SUBRESOURCE_DATA textureSubresourceData;
+    ZeroMemory(&textureSubresourceData, sizeof textureSubresourceData);
+    textureSubresourceData.pSysMem = testTextureBytes;
+    textureSubresourceData.SysMemPitch = texBytesPerRow;
+
+    ID3D11Texture2D* texture;
+    ID3D11Device_CreateTexture2D(ctx->dev, &textureDesc, &textureSubresourceData, &texture);
+
+    ID3D11ShaderResourceView* textureView;
+    ZeroMemory(&textureView, sizeof textureView);
+    ID3D11Device_CreateShaderResourceView(ctx->dev, texture, NULL, &textureView);
 
     iupAttribSet(ih, "ERROR", NULL);
     ID3D10Blob_Release(vs);
@@ -245,6 +305,30 @@ static int CanvasDefaultResize_CB(Ihandle* ih, int width, int height)
     D3D11_VIEWPORT viewport;
     Context3D* ctx = (Context3D*)iupAttribGet(ih, "_IUP_D3D11CONTEXT");
 
+    ID3D11DeviceContext_OMSetRenderTargets(ctx->devcon, 0, 0, 0);
+    ID3D11RenderTargetView_Release(ctx->backbuffer);
+    ID3D11DeviceContext_Flush(ctx->devcon);
+
+    switch (IDXGISwapChain_ResizeBuffers(ctx->swapchain, 1, width, height, DXGI_FORMAT_UNKNOWN, 0))
+    {
+    case 0: break;
+    case DXGI_ERROR_DEVICE_REMOVED:
+    case DXGI_ERROR_DEVICE_RESET:
+        log_err("device removed/reset\n");
+        break;
+    default:
+        log_err("Failed to resize\n");
+        break;
+    }
+
+    {
+        ID3D11Texture2D* backbuffer;
+        IDXGISwapChain_GetBuffer(ctx->swapchain, 0, &IID_ID3D11Texture2D, &backbuffer);
+
+        ID3D11Device_CreateRenderTargetView(ctx->dev, (ID3D11Resource*)backbuffer, NULL, &ctx->backbuffer);
+        ID3D11Texture2D_Release(backbuffer);
+    }
+
     ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
     viewport.TopLeftX = 0;
     viewport.TopLeftY = 0;
@@ -254,9 +338,39 @@ static int CanvasDefaultResize_CB(Ihandle* ih, int width, int height)
     viewport.MaxDepth = 1.0;
 
     ID3D11DeviceContext_RSSetViewports(ctx->devcon, 1, &viewport);
-    Iup3DSwapBuffers(ih);
 
     return IUP_DEFAULT;
+}
+
+static int CanvasDefaultRedraw_CB(Ihandle* ih, float x, float y)
+{
+    Context3D* ctx = (Context3D*)iupAttribGet(ih, "_IUP_D3D11CONTEXT");
+    log_dbg("Redraw\n");
+
+    ID3D11DeviceContext_OMSetRenderTargets(ctx->devcon, 1, &ctx->backbuffer, NULL);
+    ID3D11DeviceContext_IASetPrimitiveTopology(ctx->devcon, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    ID3D11DeviceContext_IASetInputLayout(ctx->devcon, ctx->quad_layout);
+
+    ID3D11DeviceContext_VSSetShader(ctx->devcon, ctx->vs, NULL, 0);
+    ID3D11DeviceContext_PSSetShader(ctx->devcon, ctx->ps, NULL, 0);
+
+    UINT stride = sizeof(struct Vertex);
+    UINT offset = 0;
+    ID3D11DeviceContext_IASetVertexBuffers(ctx->devcon, 0, 1, &ctx->quad, &stride, &offset);
+
+    D3D11_MAPPED_SUBRESOURCE mappedSubresource;
+    d3d11DeviceContext->Map(constantBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedSubresource);
+    Constants* constants = (Constants*)(mappedSubresource.pData);
+    constants->pos = { 0.25f, 0.3f };
+    constants->color = { 0.7f, 0.65f, 0.1f, 1.f };
+    d3d11DeviceContext->Unmap(constantBuffer, 0);
+    d3d11DeviceContext->VSSetConstantBuffers(0, 1, &constantBuffer);
+
+    ID3D11DeviceContext_PSSetShaderResources(ctx->devcon, 0, 1, &textureView);
+    ID3D11DeviceContext_PSSetSamplers(ctx->devcon, 0, 1, &samplerState);
+
+    ID3D11DeviceContext_Draw(ctx->devcon, 6, 0);
+    IDXGISwapChain_Present(ctx->swapchain, 0, 0);
 }
 
 static int CanvasCreateMethod(Ihandle* ih, void** params)
@@ -269,6 +383,7 @@ static int CanvasCreateMethod(Ihandle* ih, void** params)
     iupAttribSet(ih, "_IUP_D3D11CONTEXT", (char*)ctx);
 
     IupSetCallback(ih, "RESIZE_CB", (Icallback)CanvasDefaultResize_CB);
+    IupSetCallback(ih, "ACTION", (Icallback)CanvasDefaultRedraw_CB);
 
     return IUP_NOERROR;
 }
@@ -303,7 +418,21 @@ static void CanvasDestroyMethod(Ihandle* ih)
     iupAttribSet(ih, "_IUP_D3D11CONTEXT", NULL);
 }
 
-void iupdrv3DCanvasInitClass(Iclass* ic)
+int iupdrvGfxInit(void)
+{
+    return 0;
+}
+
+void iupdrvGfxDeInit(void)
+{
+}
+
+void iupdrvGfxSetTexture(Ihandle* ih, int id, const char* value)
+{
+
+}
+
+void iupdrvGfxCanvasInitClass(Iclass* ic)
 {
     ic->Create = CanvasCreateMethod;
     ic->Map = CanvasMapMethod;
@@ -311,37 +440,23 @@ void iupdrv3DCanvasInitClass(Iclass* ic)
     ic->Destroy = CanvasDestroyMethod;
 }
 
-int Iup3DIsCurrent(Ihandle* ih)
+int IupGfxIsCurrent(Ihandle* ih)
 {
     (void)ih;
     return 1;
 }
 
-void Iup3DMakeCurrent(Ihandle* ih)
+void IupGfxMakeCurrent(Ihandle* ih)
 {
     (void)ih;
 }
 
 static FLOAT CLEAR_COLOR[4] = { 0.0f, 0.2f, 0.4f, 1.0f };
 
-void Iup3DSwapBuffers(Ihandle* ih)
+void IupGfxSwapBuffers(Ihandle* ih)
 {
     Context3D* ctx = (Context3D*)iupAttribGet(ih, "_IUP_D3D11CONTEXT");
     log_dbg("Swap buffers\n");
 
-    ID3D11DeviceContext_ClearRenderTargetView(ctx->devcon, ctx->backbuffer, CLEAR_COLOR);
-
-    ID3D11DeviceContext_OMSetRenderTargets(ctx->devcon, 1, &ctx->backbuffer, NULL);
-    ID3D11DeviceContext_IASetPrimitiveTopology(ctx->devcon, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-    ID3D11DeviceContext_IASetInputLayout(ctx->devcon, ctx->quad_layout);
-
-    UINT stride = sizeof(struct Vertex);
-    UINT offset = 0;
-    ID3D11DeviceContext_IASetVertexBuffers(ctx->devcon, 0, 1, &ctx->quad, &stride, &offset);
-
-    ID3D11DeviceContext_VSSetShader(ctx->devcon, ctx->vs, NULL, 0);
-    ID3D11DeviceContext_PSSetShader(ctx->devcon, ctx->ps, NULL, 0);
-
-    ID3D11DeviceContext_Draw(ctx->devcon, 6, 0);
     IDXGISwapChain_Present(ctx->swapchain, 0, 0);
 }
